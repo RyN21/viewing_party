5c945b7d77b92b6496e5b853ef5dd85a58d58a52
Alex Lynch <mlynch5187@gmail.com>
2020-08-17 17:50:58 -0600

    Create welcome view

--- spec/features/welcome/visitor_can_see_welcome_spec.rb
+++ spec/features/welcome/visitor_can_see_welcome_spec.rb
@@ -4,7 +4,8 @@ RSpec.describe "Welcome page" do
   it "as a visitor I can see the welcome page" do
       visit "/"
 
-      expect(page).to have_content("Welcome to the Viewing Party! This app streamlines your video watching experience!")
+      expect(page).to have_content("Welcome to the Viewing Party!")
+      expect(page).to have_content("This app streamlines your video watching experience!")
       expect(page).to have_link("Login")
   end
 end
