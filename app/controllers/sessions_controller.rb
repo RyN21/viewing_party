f640f39361c7e7c14e7dd1923b35eae506ecb8ab
Alex Lynch <mlynch5187@gmail.com>
2020-08-19 17:39:15 -0600

    Rubocop clean up - law abiding citizens!
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    access_token = request.env["omniauth.auth"]
+    access_token = request.env['omniauth.auth']
     user = User.from_omniauth(access_token)
     session[:user_id] = user.id
     user.google_token = access_token.credentials.token
@@ -8,8 +8,5 @@ class SessionsController < ApplicationController
     user.google_refresh_token = refresh_token if refresh_token.present?
     user.save
     redirect_to root_path
-    binding.pry
   end
 end
-
-# current uer in application record
