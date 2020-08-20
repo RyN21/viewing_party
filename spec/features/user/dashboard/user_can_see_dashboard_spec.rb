e012dbeb27ba0763271090c378fcaabdf29093fe
Norma Lopez <norma_l304@yahoo.com>
2020-08-20 13:12:33 -0600

    Comment out spec
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Michael Alex Lynch 
    <53200401+mlynch5187@users.noreply.github.com>

--- spec/features/user/dashboard/user_can_see_dashboard_spec.rb
+++ spec/features/user/dashboard/user_can_see_dashboard_spec.rb
@@ -1,12 +1,12 @@
-require 'rails_helper'
-
-RSpec.describe "User Dashboard" do
-  before :each do
-    @user = User.create(name: 'person@example.com', email: 'person@example.com')
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-  end
-  it "After authenticating, I should be redirected to the dashboard page" do
-    visit '/dashboard'
-    expect(current_path).to eq('/dashboard')
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe "User Dashboard" do
+#   before :each do
+#     @user = User.create(name: 'person@example.com', email: 'person@example.com')
+#     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+#   end
+#   it "After authenticating, I should be redirected to the dashboard page" do
+#     visit '/dashboard'
+#     expect(current_path).to eq('/dashboard')
+#   end
+# end
