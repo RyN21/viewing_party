7f4ac0e54777fd8354ae1c88a02e5f921df513a5
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-20 16:54:59 -0600

    Add discover controller and discover index
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>
    Co-authored-by: Michael Alex Lynch <53200401+mlynch5187@users.noreply.github.com>

--- app/controllers/user/discover_controller.rb
+++ app/controllers/user/discover_controller.rb
@@ -1,8 +1,8 @@
 class User::DiscoverController < ApplicationController
   def index
     conn = Faraday.new('https://api.themoviedb.org/3') do |f|
-      f.params['api_key'] = ENV["MOVIE_API_KEY"]
+      f.params['api_key'] = ENV['MOVIE_API_KEY']
     end
-    require "pry"; binding.pry
+    conn
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
 
-
   def self.from_omniauth(auth)
     where(email: auth.info.email).first_or_initialize do |user|
       user.name = auth.info.name
