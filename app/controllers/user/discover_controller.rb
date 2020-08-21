891e855ca52f5813c1499670f093980ef597274c
Alex Lynch <mlynch5187@gmail.com>
2020-08-20 18:46:21 -0600

    Create movie search method
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>

--- app/controllers/user/discover_controller.rb
+++ app/controllers/user/discover_controller.rb
@@ -1,8 +1,18 @@
 class User::DiscoverController < ApplicationController
-  def index
-    conn = Faraday.get('https://api.themoviedb.org/3/movie/top_rated') do |f|
+  def top_rated
+    response = Faraday.get('https://api.themoviedb.org/3/movie/top_rated') do |f|
       f.params['api_key'] = ENV['MOVIE_KEY']
     end
-    require "pry"; binding.pry
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def movie_search(movie)
+    movie = params[:movie]
+
+    response = Faraday.get('https://api.themoviedb.org/3/search/movie') do |f|
+      f.params['api_key'] = ENV['MOVIE_KEY']
+      f.params['query'] = movie
+    end
+    json = JSON.parse(response.body, symbolize_names: true)
   end
 end
