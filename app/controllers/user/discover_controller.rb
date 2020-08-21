4a6620828430f571adb12c31ee2f9d28442434fb
Alex Lynch <mlynch5187@gmail.com>
2020-08-20 18:25:01 -0600

    Update discover index conn url
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>

--- app/controllers/user/discover_controller.rb
+++ app/controllers/user/discover_controller.rb
@@ -1,9 +1,8 @@
 class User::DiscoverController < ApplicationController
   def index
-    conn = Faraday.get('https://api.themoviedb.org/3/movie/30') do |f|
+    conn = Faraday.get('https://api.themoviedb.org/3/movie/top_rated') do |f|
       f.params['api_key'] = ENV['MOVIE_KEY']
     end
     require "pry"; binding.pry
-    # conn
   end
 end
