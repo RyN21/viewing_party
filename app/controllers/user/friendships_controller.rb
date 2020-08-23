a7f1ae868ccc770d88b2a9ef79f1cb517a0bf901
Alex Lynch <mlynch5187@gmail.com>
2020-08-23 13:46:53 -0600

    Friendship mechanics WIP
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>

--- app/controllers/user/friendships_controller.rb
+++ app/controllers/user/friendships_controller.rb
@@ -2,7 +2,6 @@ class User::FriendshipsController < ApplicationController
   protect_from_forgery with: :exception
 
   def create
-    friend_email = params[:user][:email]
     @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
     redirect_to user_path
   end
