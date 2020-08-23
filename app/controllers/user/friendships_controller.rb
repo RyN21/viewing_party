952c38fb275325844b0320cb097b2623e54ab937
Alex Lynch <mlynch5187@gmail.com>
2020-08-23 13:02:19 -0600

    Add friend_id to friendships table Setup friendship model with methods
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>

--- app/controllers/user/friendships_controller.rb
+++ app/controllers/user/friendships_controller.rb
@@ -1,15 +1,10 @@
-class FriendshipsController < ApplicationController
+class User::FriendshipsController < ApplicationController
   protect_from_forgery with: :exception
 
   def create
-    if params.include?(:friend_id)
-      @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, params[:friend_id])
-    else
-      params[:user][:friend_ids].each do |f_id|
-      @new_friendships = Friendship.create_reciprocal_for_ids(current_user_id, f_id)
-      end
-    end
-    redirect_to users_path
+    friend_email = params[:user][:email]
+    @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
+    redirect_to user_path
   end
 
   def destroy
