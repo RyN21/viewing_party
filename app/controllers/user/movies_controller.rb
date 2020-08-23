5528d3aa8c416961e2fd32a49ee5dab8303bca1e
Alex Lynch <mlynch5187@gmail.com>
2020-08-22 19:31:18 -0600

    utilize conn method in movies_controller show method fetch requests

--- app/controllers/user/movies_controller.rb
+++ app/controllers/user/movies_controller.rb
@@ -8,19 +8,19 @@ class User::MoviesController < ApplicationController
 
   def show
     movie_id = params[:id].to_i
-    movie_response = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}") do |f|
+    movie_response = conn.get("movie/#{movie_id}") do |f|
       f.params['api_key'] = ENV['MOVIE_KEY']
       f.params['movie_id'] = movie_id
     end
 
     @movie = JSON.parse(movie_response.body, symbolize_names: true)
-    credit_response = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}/credits") do |f|
+    credit_response = conn.get("movie/#{movie_id}/credits") do |f|
       f.params['api_key'] = ENV['MOVIE_KEY']
       f.params['movie_id'] = movie_id
     end
 
     @credit = JSON.parse(credit_response.body, symbolize_names: true)
-    review_response = Faraday.get("https://api.themoviedb.org/3/movie/#{movie_id}/reviews") do |f|
+    review_response = conn.get("movie/#{movie_id}/reviews") do |f|
       f.params['api_key'] = ENV['MOVIE_KEY']
       f.params['movie_id'] = movie_id
     end
