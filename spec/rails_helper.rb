5220b5a25d34ec024bfde207092eb19680312035
Norma Lopez <norma_l304@yahoo.com>
2020-08-22 23:58:49 -0600

    Add #mock_auth_hash which mocks authorized google user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,4 +62,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+def mock_auth_hash
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+    'provider' => 'google_oauth2',
+    'uid' => '123545',
+    'info' => {
+      'name' => 'person@example.com',
+      'email' => 'person@example.com'
+    },
+    'credentials' => {
+      'token' => 'abcdefg12345',
+      'refresh_token' => '12345abcdefg'
+    }
+  })
 end
