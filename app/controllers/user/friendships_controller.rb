88c684cc904239792c27bcf5fbad3bf45a09a753
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-08-24 18:34:52 -0600

    Add to freindship controller
    Co-authored-by: Norma Lopez <57690961+IamNorma@users.noreply.github.com>
    Co-authored-by: Michael Alex Lynch 
    <53200401+mlynch5187@users.noreply.github.com>

--- app/controllers/user/friendships_controller.rb
+++ app/controllers/user/friendships_controller.rb
@@ -2,6 +2,9 @@ class User::FriendshipsController < ApplicationController
   protect_from_forgery with: :exception
 
   def create
+    friend_email = params[:user][:email]
+    user = User.find_by(username: friend_email)
+    require "pry"; binding.pry
     @new_friendships = Friendship.create_reciprocal_for_ids(current_user.id, params[:friend_id])
     redirect_to user_path
   end
