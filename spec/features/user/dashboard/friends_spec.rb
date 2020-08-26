b3d84bd7eace80c6d0e3f6141312037c46d00b02
Norma Lopez <norma_l304@yahoo.com>
2020-08-26 00:08:19 -0600

    Test sad path for adding friends

--- spec/features/user/dashboard/friends_spec.rb
+++ spec/features/user/dashboard/friends_spec.rb
@@ -33,8 +33,18 @@ RSpec.describe "Friends" do
       fill_in :username, with: "#{@user3.username}"
       click_button "Add Friend"
     end
-    
+
     expect(current_path).to eq('/user')
     expect(page).to have_content("#{@user1.username}")
+  end
+
+  it "user cannot add friend if friend is not a registered user" do
+    within '.friends' do
+      fill_in :username, with: "example@turing.io"
+      click_button "Add Friend"
+    end
+
+    expect(current_path).to eq(user_path)
+    expect(page).to have_content("Could not find user with that email")
   end
 end
